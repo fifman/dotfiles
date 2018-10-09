b0fa81f161b70dfff99e80e1e0245bedeb915761
Yicheng Feng <1264380449@qq.com>
2018-10-09 10:29:44 +0800

    new

--- oh-my-zsh/custom/plugins/powerline/powerline.plugin.zsh
+++ oh-my-zsh/custom/plugins/powerline/powerline.plugin.zsh
@@ -1,8 +1,6 @@
 function init_powerline {
-    powerline_path=~/Library/Python/2.7
-    export PATH="$powerline_path/bin:$PATH"
     powerline-daemon -q
-    . $powerline_path/lib/python/site-packages/powerline/bindings/zsh/powerline.zsh
+    . /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
 }
 
 after_hooks+=(init_powerline)
